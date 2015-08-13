85bdbebfacf7ad88acb675a2f8532b2e94ad7518
Marcel Hellkamp <mhellka@gwdg.de>
2015-08-13 15:22:15 +0200

    Added an XML structure comparison that works well with randomized UUIDs in attributes.

--- src/test/java/eu/pericles/modelcompiler/testutils/Utils.java
+++ src/test/java/eu/pericles/modelcompiler/testutils/Utils.java
@@ -3,8 +3,13 @@ package eu.pericles.modelcompiler.testutils;
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
+
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
+import org.xml.sax.SAXException;
 
 import eu.pericles.modelcompiler.common.BpmnGenericConversor;
 import eu.pericles.modelcompiler.common.BpmnJbpmConversor;
@@ -73,7 +78,7 @@ public class Utils {
 		try {
 			String a = FileUtils.readFileToString(new File(outputFileName));
 			String b = FileUtils.readFileToString(new File(testFileName));
-			Assert.assertEquals(a,b);
+			XMLCompare.assertEquals(a, b);
 		} catch (IOException e) {
 			Assert.fail(e.getMessage());
 		}
