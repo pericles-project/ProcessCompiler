5ea66a1723ae02534ffcbbf6593adea30c87a125
Noa Campos Lopez <ncampos@gwdg.de>
2015-11-06 18:31:41 +0100

    Request the the datastore ERMR with credentials

--- src/test/java/eu/pericles/processcompiler/unittests/ERMRRequestsTests.java
+++ src/test/java/eu/pericles/processcompiler/unittests/ERMRRequestsTests.java
@@ -16,33 +16,78 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation.Builder;
 
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.Test;
 
 public class ERMRRequestsTests {
-	
-	final String ermr = "https://141.5.102.86/api/triple";
+
+	final String tripleStore = "https://141.5.102.86/api/triple";
+	final String objectStore = "https://141.5.102.86/api/cdmi";
+	final String user = "pericles1";
+	final String password = "Per1cles1Pw";
 
 	@Test
-	public void test() {
-		String repo = "testJerome";
+	public void getCollection() {
+		String collection = "NoaCollection";
 
-		try {	
-			Client client = ClientBuilder.newBuilder()
-					.hostnameVerifier(getAllTrustHostnameVerifier())
-					.sslContext(getAllTrustSSLContext())
-					.build();
-			
-			Builder builder = client.target(ermr).path(repo).request();
-			
+		try {
+			Client client = getAllTrustingClient();
+			client.register(HttpAuthenticationFeature.basic(user, password));
+
+			Builder builder = client.target(objectStore).path(collection).request();
+
 			String response = builder.get(String.class);
 			System.out.println("Response: " + response);
-			
+
+			client.close();
+
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
 	
-	private HostnameVerifier getAllTrustHostnameVerifier() {
+	@Test
+	public void getRepository() {
+		String repository = "testJerome";
+
+		try {
+			Client client = getAllTrustingClient();
+
+			Builder builder = client.target(tripleStore).path(repository).request();
+
+			String response = builder.get(String.class);
+			System.out.println("Response: " + response);
+
+			client.close();
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * Returns a Client instance for a RESTful API that disables the certificate
+	 * validation in Java SSL connections with self-signed servers. The client
+	 * is built through a ClientBuilder with an all-trusting hostname verifier
+	 * and an all-trusting trust manager
+	 * 
+	 * @return a Client that trusts all host names and credentials in SSL
+	 *         connections
+	 * @throws KeyManagementException
+	 * @throws NoSuchAlgorithmException
+	 */
+	private Client getAllTrustingClient() throws KeyManagementException, NoSuchAlgorithmException {
+		return ClientBuilder.newBuilder().hostnameVerifier(getAllTrustingHostnameVerifier()).sslContext(getAllTrustingSSLContext()).build();
+	}
+
+	/**
+	 * Returns a host name verifier that trust all host names by returning
+	 * always true when calling the function verify(String hostname, SSLSession
+	 * session)
+	 * 
+	 * @return a HostnameVerifier trusting all host names
+	 */
+	private HostnameVerifier getAllTrustingHostnameVerifier() {
 		HostnameVerifier hostnameVerifier = new HostnameVerifier() {
 			public boolean verify(String hostname, SSLSession session) {
 				return true;
@@ -51,21 +96,38 @@ public class ERMRRequestsTests {
 		return hostnameVerifier;
 	}
 
-	private SSLContext getAllTrustSSLContext() throws KeyManagementException, NoSuchAlgorithmException {
+	/**
+	 * Returns a SSL context where all the credentials are trusted by creating a
+	 * trust manager that does not validate certificate chains
+	 * 
+	 * @return a SSLContext where the TrustManager trusts everything
+	 * @throws KeyManagementException
+	 * @throws NoSuchAlgorithmException
+	 */
+	private SSLContext getAllTrustingSSLContext() throws KeyManagementException, NoSuchAlgorithmException {
 		SSLContext sslContext = SSLContext.getInstance("SSL");
-		sslContext.init(null, getAllTrustManager(), new SecureRandom());
+		sslContext.init(null, getAllTrustingTrustManager(), new SecureRandom());
 		return sslContext;
 	}
-	
-	private TrustManager[] getAllTrustManager() {
-		TrustManager[] trustAllManager = new TrustManager[] {
-				new X509TrustManager() {
-					public X509Certificate[] getAcceptedIssuers() { return null; }
-					public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-					public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-					}
-				};
-		return trustAllManager;
+
+	/**
+	 * Returns a trust manager that does not validate certificate chains
+	 * 
+	 * @return a TrustManager that trusts all credentials
+	 */
+	private TrustManager[] getAllTrustingTrustManager() {
+		TrustManager[] allTrustManager = new TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+		} };
+		return allTrustManager;
 	}
 
 }
