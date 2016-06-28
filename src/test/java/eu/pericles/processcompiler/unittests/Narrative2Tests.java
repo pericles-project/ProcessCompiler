2926cfd8192fa9500fe8687bbcb974ce80805d3d
Noa Campos Lopez <ncampos@gwdg.de>
2016-06-28 12:00:23 +0200

    Start Narrative2 tests

---
+++ src/test/java/eu/pericles/processcompiler/unittests/Narrative2Tests.java
@@ -0,0 +1,14 @@
+package eu.pericles.processcompiler.unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Narrative2Tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
